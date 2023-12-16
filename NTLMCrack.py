6b317718d43e672856758051cd30afea68f2f69a
Adamkadaban <adamkadaban@gmail.com>
2023-12-16 05:12:15 -0500

    added some logging fixed logic with filtering

--- NTLMCrack.py
+++ NTLMCrack.py
@@ -4,6 +4,10 @@ import requests
 import argparse
 import sys
 
+GREEN = '\033[92m'
+RED = '\033[91m'
+BLUE = '\033[94m'
+RESET = '\033[0m'
 
 def crack_hashes(path, n):
 	pattern = r'(.*):\d+:[a-f0-9]{32}:([a-f0-9]{32}):::'
@@ -15,13 +19,15 @@ def crack_hashes(path, n):
 		with open(path) as fin:
 			for idx, line in enumerate(fin, start=1):
 				match = re.match(pattern, line.rstrip())
-				users.append(match.group(1))
-				hashes.append(match.group(2))
-
+				if match:
+					users.append(match.group(1))
+					hashes.append(match.group(2))
+				else:
+					print(f"{RED}[-] Couldn't match on line:{RESET} {line}")
 				if idx == n:
 					break
 	except FileNotFoundError:
-		print(f'File "{path}" not found')
+		print(f'{RED}[-] File "{path}" not found')
 		sys.exit(1)
 
 	hashPhases = [hashes[i: i + 100] for i in range(0, len(hashes), 100)]
@@ -40,7 +46,7 @@ def crack_hashes(path, n):
 
 		cracked.extend([c.split(':')[1] for c in r_text.split("\n")[:-1]])
 
-	return dict(zip(users, cracked))
+	return len(users), dict(zip(users, cracked))
 
 def main():
 	parser = argparse.ArgumentParser(description="Use ntlm.pw to automatically convert hash dumps to credentials")
@@ -55,22 +61,35 @@ def main():
 
 	args = parser.parse_args()
 
-	cracked = crack_hashes(args.file_path, args.n)
+	if args.filter_not_found:
+		print(f"{BLUE}[*] Filtering hashes that can't be cracked{RESET}")
+	if args.n:
+		print(f"{BLUE}[*] Limiting to first {args.n} hashes{RESET}")
+
+	toCrackLen, cracked = crack_hashes(args.file_path, args.n)
+	totCracked = 0
 
 	if args.separate_files:
 		userFile = open('users', 'w')
 		passFile = open('passwords', 'w')
 		for user, password in cracked.items():
-			if not args.filter_not_found and password != '[not found]':
+			if not args.filter_not_found or password != '[not found]':
 				userFile.write(user + "\n")
 				passFile.write(password + "\n")
+			if password != '[not found]':
+				totCracked += 1
 		userFile.close()
 		passFile.close()
+		print(f'{GREEN}[+] Wrote users/passwords to files: "users" and "passwords"{RESET}')
 	else:
 		with open('credentials', 'w') as fout:
 			for user, password in cracked.items():
-				if not args.filter_not_found and password != '[not found]':
+				if not args.filter_not_found or password != '[not found]':
 					fout.write(user + ":" + password + "\n")
+				if password != '[not found]':
+					totCracked += 1
+		print(f'{GREEN}[+] Wrote credentials to file: "credentials"{RESET}')
 
+	print(f'{GREEN}[+] Cracked {totCracked}/{toCrackLen} hashes{RESET}')
 if __name__ == "__main__":
 	main()
